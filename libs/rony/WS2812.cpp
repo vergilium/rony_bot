693122ce9ed2f398df550e454c93e821a917a428
Bodkar <bhhuuu18@gmail.com>
2024-11-21 12:27:03 +0200

    Update

--- libs/rony/WS2812.cpp
+++ libs/rony/WS2812.cpp
@@ -5,9 +5,9 @@ WS2812::WS2812() {
     _strip.show();
 }
 
-void WS2812::setColor(uint8_t r, uint8_t g, uint8_t b, uint8_t index) {
+void WS2812::setColor(uint8_t rgb[3], uint8_t index) {
     if (index <= RGB_CNT) {
-        _strip.setPixelColor(index, _strip.Color(r, g, b));
+        _strip.setPixelColor(index, _strip.Color(rgb[0], rgb[1], rgb[2]));
     }
     _strip.show();
 }
--- libs/rony/WS2812.h
+++ libs/rony/WS2812.h
@@ -3,6 +3,8 @@
 
 #include "Adafruit_NeoPixel.h"
 
+#include "Arduino.h"
+
 //
 // RGB LEDs WS2812
 //
@@ -14,14 +16,10 @@
 #define RGB_PIN 23
 #define RGB_CNT 4
 
-#include "Arduino.h"
-
-#include "Adafruit_NeoPixel.h"
-
 class WS2812 /*: public Adafruit_NeoPixel*/ {
     public:
         WS2812();
-        void setColor(uint8_t r, uint8_t g, uint8_t b, uint8_t index);
+        void setColor(uint8_t rgb[3], uint8_t index);
     private:
         Adafruit_NeoPixel _strip = Adafruit_NeoPixel(RGB_CNT, RGB_PIN, NEO_GRB + NEO_KHZ800);
 };
